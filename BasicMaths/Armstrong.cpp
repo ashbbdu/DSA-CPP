7c721160091fca9d776257a7eb519b0cb859a75f
Ashish Srivastava <ashishsrivastava7007@gmail.com>
2024-02-23 00:33:52 +0530

    armstrong number

--- BasicMaths/Armstrong.cpp
+++ BasicMaths/Armstrong.cpp
@@ -3,15 +3,21 @@ using namespace std;
 
 int main () {
     int num = 153;
+    int originalNumber = num;
     int dup = num;
     int sum = 0;
+    int totalNum = 0;
+    while(dup > 0) {
+        totalNum++;
+        dup = dup / 10;
+    }
     while (num > 0)
     {
-       int lastDigit = num % 10;
-       sum = sum + (lastDigit * lastDigit * lastDigit);
+    int lastDigit = num % 10;
+      sum = sum + pow(lastDigit , totalNum);
        num = num / 10;
     }
-     if(sum == dup){
+     if(sum == originalNumber){
         cout << "Yes" << endl;
     } else {
         cout << "No" << endl;
