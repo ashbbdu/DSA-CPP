2185c102ca78b2c0d6388668f77f0807972fa176
Ashish Srivastava <ashishsrivastava7007@gmail.com>
2024-03-16 12:29:43 +0530

    stacks and queue basics

---
+++ STL/Stack.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+using namespace std;
+
+int main () {
+    // Last in first out (LIFO)
+    // swap
+    stack <int> st;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.pop();
+    cout << st.top() <<  " " << st.size() << endl;
+
+    // Queue
+    // FIFO
+    queue <int> qu;
+    qu.push(10);
+    qu.push(2);
+    qu.push(3);
+    qu.push(4);
+    cout << qu.front() << " " << qu.back();
+}
